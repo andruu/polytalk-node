8edc76e961d35db7b990805eacb08f93be71f810
andruu <andru.weir@gmail.com>
2012-11-30 15:50:29 -0500

    Adding output displaying port and pid

--- examples/server.js
+++ examples/server.js
@@ -7,16 +7,16 @@ var Test = {};
 
 Test.add = function (a, b) {
   return a + b;
-}
+};
 
 Test.shout = function (words) {
   return (words + '').toUpperCase();
-}
+};
 
 exposes['Test'] = Test;
 
 var server = new Polytalk.Server({ port: 9090, exposes: exposes });
-
+console.log("Node Server running on port 9090. PID=" + process.pid);
 server.run(function(connection, request) {
   server.call(request, function (response) {
     server.push(connection, response);
--- examples/server_coffee.coffee
+++ examples/server_coffee.coffee
@@ -12,7 +12,7 @@ class Test
 exposes['Test'] = Test
 
 server = new Polytalk.Server port: 9090, exposes: exposes
-
+console.log "Node Coffee Server running on port 9090. PID=#{process.pid}"
 server.run (connection, request) ->
   server.call request, (response) ->
     server.push(connection, response)
